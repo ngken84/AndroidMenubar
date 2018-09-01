d735e0034873c120aeeb56cab943ce958b943c23
Ken Ng <ng.ken84@gmail.com>
2018-08-31 22:28:00 -0700

    Implement Menu Icon and Actions

--- app/src/main/java/com/example/ngken/menubarexample/MenuIconActivity.java
+++ app/src/main/java/com/example/ngken/menubarexample/MenuIconActivity.java
@@ -2,6 +2,9 @@ package com.example.ngken.menubarexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MenuIconActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MenuIconActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_icon);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.iconmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.iconMenu:
+                Toast.makeText(getApplicationContext(), "MenuIcon Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
