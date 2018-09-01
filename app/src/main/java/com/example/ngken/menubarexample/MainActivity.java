b789008759b7147e7a15bf2a0cd6bf90c87104f8
Ken Ng <ng.ken84@gmail.com>
2018-08-31 21:38:25 -0700

    Add toast to menu item select

--- app/src/main/java/com/example/ngken/menubarexample/MainActivity.java
+++ app/src/main/java/com/example/ngken/menubarexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ngken.menubarexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuMain1:
+                Toast.makeText(getApplicationContext(), "Option1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuMain2:
+                Toast.makeText(getApplicationContext(), "Option2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuMain3:
+                Toast.makeText(getApplicationContext(), "Option3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
